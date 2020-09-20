7a10a81040500b0c98cfcb302d8884b3e3d8c1a4
Bunmi10 <Bunmi10>
2020-09-20 22:04:44 +0100

    General Practice

---
+++ runs.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+sudo addgroup pupils
+"add group"
+
+sudo adduser Amy
+"create a new user"
+
+touch runs.sh
+"create a newfile"
+
+echo "hello" > runs.sh
+"enter and echo command"
+
+chmod g=x
+"file executable only by member of group"
+
+./runs.sh
+"to run file"
+#End
+
+
